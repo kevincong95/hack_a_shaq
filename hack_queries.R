30a136252b53c70551f7368f722f3cf4f8850753
kevincong95 <kevincong95@berkeley.edu>
2017-02-04 17:12:25 -0800

    affected games

--- hack_queries.R
+++ hack_queries.R
@@ -9,3 +9,11 @@ allPlayers <- allPlayers[allPlayers$ROSTERSTATUS == 1,]
 allPlayers <- allPlayers[order(allPlayers$FT_RATE15),]
 vulnerable <- allPlayers[allPlayers$FT_RATE15 > 0,]
 vulnerable <- vulnerable[vulnerable$FT_RATE15 < 0.7,]
+#new for Spring 2017
+bestTargets <- as.numeric(names(targets))
+majorHacks <- hacks[is.element(hacks$HackedPlayerID, bestTargets), ] #only consider hacking of most popular targets from here on out
+hackers <- table(majorHacks$Team)
+hackees <- table(majorHacks$Opp)
+frequentHackers <- hackers[which(hackers >= 20)]
+criticalGames <- majorHacks[is.element(majorHacks$Team, names(frequentHackers)), ]
+affectedGames <- unique(criticalGames$GameID)
